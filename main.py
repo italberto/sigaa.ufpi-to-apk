37ef601ed4d7605e0dbb5c67d1d51e5a31abee65
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2018-03-12 23:21:19 -0300

    função de login e alterações no painel de usuário

--- main.py
+++ main.py
@@ -12,20 +12,26 @@ import requests
 class Gerenciador(ScreenManager):
     pass
 
+class Menu(Screen):
+    pass
+
 class PopErrorConection(Popup):
     """Caso a função de login falhe em se conectar com o servidor do sigaa"""
     pass
     
 class PopSenhaErrada(Popup):
+    """Caso a senha ou usuários estejam errados"""
     pass
 
 class DoLogin(Screen):
     
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-    
     usuario_input = ObjectProperty()
     senha_input = ObjectProperty()
+    
+    def __init__(self, **kwargs):
+        super(DoLogin, self).__init__(**kwargs)
+    
+    
     
     def logar_se(self):
         #pego a entrada o user
@@ -37,14 +43,27 @@ class DoLogin(Screen):
             self.login = pegar_id_autenticado( usuario, senha)
             self.session_id = self.login[0]
             self.resposta = self.login[1]
+            
+            #Caso usuário ou senha esteja errada ,um popup é chamado
+            if self.resposta.text.find('Usuário e/ou senha inválidos') >= 0:
+                PopSenhaErrada().open()
+                print('Usuário e/ou senha inválidos')
+                
+                return False, None
+            else:
+                #caso a senha esteja correta chamamos a tela de menu
+                self.parent.current = 'menu'
+                return True, self.session_id
+
         except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
-            PopError().open()
+            PopErrorConection().open()
 
             print('erro de connection')
+        
+        
+        
             
-        if self.resposta.text.find('Usuário e/ou senha inválidos') >= 0:
-            PopSenhaErrada().open()
-            print('Usuário e/ou senha inválidos')
+        
     
     
         
